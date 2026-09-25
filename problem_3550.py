564e952fc61c6f181b1e6ad8d1b975920e1b5a56
ANTHONY8652 <githinjianthony720@gmail.com>
2026-09-25 17:05:37 +0300

    Updates

--- problem_3550.py
+++ problem_3550.py
@@ -13,3 +13,15 @@ def SmallestIndex(nums):
 print(SmallestIndex([1,5,6,4]))
 print(SmallestIndex([2,3,1]))
 print(SmallestIndex([1,10,11]))
+
+"""
+
+class Solution:
+    def smallestIndex(self, nums:list[int]) -> int:
+        for i, num in enumerate(nums):
+            if sum(int(d) for d in str(num)) == i:
+                return i
+        return -1
+
+[1,3,2]
+"""
