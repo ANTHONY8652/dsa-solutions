59c7227cf1ba97b94741c335c735204f8e8d94c1
ANTHONY8652 <githinjianthony720@gmail.com>
2026-09-25 16:07:38 +0300

    day one

---
+++ problem_3550.py
@@ -0,0 +1,15 @@
+# 3550. Smallest Index With Digit Sum Equal to Index
+# Approach: Loop through array, calculate digit sum using str conversion
+# Time complexity: O(n)
+
+def SmallestIndex(nums):
+    for i in range(len(nums)):
+        digit_sum = sum(int(d) for d in str(nums[i]))
+
+        if digit_sum == i:
+            return i
+    return -1
+
+print(SmallestIndex([1,5,6,4]))
+print(SmallestIndex([2,3,1]))
+print(SmallestIndex([1,10,11]))
